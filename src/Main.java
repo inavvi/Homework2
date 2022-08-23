0028b849124581ccdfeb968c14954eb72cfb7ef2
Ivan Votintsev <navvi@mail.ru>
2022-08-23 23:34:51 +0300

    Домашнее задание 2.1

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog=8.0;
+        var cat=3.6;
+        var paper=763789;
+        dog=dog+4;
+        System.out.println(dog);
+        dog=dog-3.6;
+        System.out.println(dog);
+        cat=cat+4;
+        paper=paper+4;
+        var cat2=cat-1.6;
+        var peper2=paper-7639;
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println(cat2);
+        System.out.println(peper2);
+    var friend=19;
+        System.out.println(friend);
+    friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var tisonWeight=78.2;
+        var aliWeight=82.7;
+        var totalWeight=tisonWeight+aliWeight;
+        System.out.println("Общий вес боксёров "+totalWeight+" кг ");
+        var weightDifference=aliWeight-tisonWeight;
+        System.out.println("Разница в весе боксеров " +weightDifference+" кг ");
+        var twoTisonWeight=tisonWeight*2;
+        var weightDifference2=totalWeight%twoTisonWeight;
+        System.out.println("Разница в весе боксеров2 " +weightDifference2+" кг ");
+        var hours=640;
+        var workerHours=8;
+        var totalWorkers=hours/workerHours;
+        System.out.println("Всего работников в компании "+totalWorkers+" человек");
+        totalWorkers=totalWorkers+94;
+        var hours2=totalWorkers*workerHours;
+        System.out.println("Если в компании работает "+totalWorkers+ " человек, то всего "+hours2+" часов работы может быть поделено между сотрудниками");
+        System.out.println("Проверьте ДЗ");
+    }
+    }
